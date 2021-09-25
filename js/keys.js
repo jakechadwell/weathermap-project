fe498611dc33e64519078daa6149b60c1266056c
jake chadwell <jake.chadwell01@gmail.com>
2021-09-25 14:56:25 -0500

    updated gitignore

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-var mapboxToken = "pk.eyJ1IjoiamFrZWNoYWR3ZWxsIiwiYSI6ImNrbmFqdGNrcTBxOG0yb21scnpudmx4eHAifQ.wDnP-knmcUAR11gvAD6naw";
-const OPEN_WEATHER_APPID = '122e686cc95b5f492e25375de8c6314b';
-let githubToken = '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';
